b0a0b4620099eb5a9d35dbcc340266e793fd1616
Jeswin James <jeswinjames24@MacBook-Pro.local>
2016-06-02 12:24:34 +0800

    Initial Commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+var server = restify.createServer();
+
+var helloBot = new builder.BotConnectorBot();
+helloBot.add('/', new builder.CommandDialog()
+    .matches('^set name', builder.DialogAction.beginDialog('/profile'))
+    .matches('^quit', builder.DialogAction.endDialog())
+    .onDefault([
+        function (session, args, next) {
+            if (!session.userData.name) {
+                session.beginDialog('/profile');
+            } else {
+                next();
+            }
+        },
+        function (session, results) {
+            session.send('Hello %s!', session.userData.name);
+        }
+    ]));
+helloBot.add('/profile',  [
+    function (session) {
+        if (session.userData.name) {
+            builder.Prompts.text(session, 'What would you like to change it to?');
+        } else {
+            builder.Prompts.text(session, 'Hi! What is your name?');
+        }
+    },
+    function (session, results) {
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
+
+server.use(helloBot.verifyBotFramework({ appId: 'you id', appSecret: 'your secret' }));
+server.post('/v1/messages', helloBot.listen());
+
+server.listen(8080, function () {
+    console.log('%s listening to %s', server.name, server.url); 
+});
